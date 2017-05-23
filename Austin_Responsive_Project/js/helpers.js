cf952c674cedbb3139f2523947ca642091a8fdd5
Jonathan Kirwan <Jonathan@ifinity.co.uk>
2017-05-23 10:49:23 +0100

    Changes to row--tagged-list-item and link__scroll-to-top

--- Austin_Responsive_Project/js/helpers.js
+++ Austin_Responsive_Project/js/helpers.js
@@ -1,17 +1,17 @@
 // Fix issue with anchor links not working due to AngularJS routing (ngRoute module)
 
-const a = window.location.href.indexOf('scroll=');
-const b = window.location.href.indexOf('&hkey=');
-const href = window.location.href.substring(a + 7, b)
+// const a = window.location.href.indexOf('scroll=');
+// const b = window.location.href.indexOf('&hkey=');
+// const href = window.location.href.substring(a + 7, b)
 
-function scrollToAnchor() {
-    if (a != -1 && b != -1) {
-        const anchor = document.querySelector(`#${href}`);
-        anchor.scrollIntoView();
-    }
-};
+// function scrollToAnchor() {
+//     if (a != -1 && b != -1) {
+//         const anchor = document.querySelector(`#${href}`);
+//         anchor.scrollIntoView();
+//     }
+// };
 
-window.onload = scrollToAnchor();
+// window.onload = scrollToAnchor();
 
 // Show / Hide scroll to top of page link
 
@@ -26,3 +26,21 @@ jQuery(window).scroll(function () {
         topLink.fadeOut();
     }
 })
+
+// Images
+
+// 'use strict';
+
+'use strict';
+
+var imgs = jQuery('.row--tagged-list-item img');
+
+if (!jQuery(imgs).length) {
+    for (i = 0; i < imgs.length; i++) {
+        var src = imgs[i].src;
+        var newSrc = src.replace(/-2x/g, '-1x');
+        console.log(newSrc);
+
+        imgs[i].setAttribute("src", '' + newSrc);
+    }
+}
